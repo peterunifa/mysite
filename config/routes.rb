050152243cf5e84d90d28ab5d002d68c7cd62241
Peter Chen <peter.chen@unifa-e.com>
2022-11-13 21:12:50 +0900

    add migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "rails/welcome#index"
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
 end
